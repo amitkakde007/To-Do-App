ee92a3443cdce5481d8d2cdcaf79351ddf8233aa
Amit Kakde <amitkakde911@outlook.com>
2020-08-10 16:51:06 +0530

    Created Task table and created a entry

--- ToDoApp/Tasks/models.py
+++ ToDoApp/Tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    Title = models.CharField(max_length=200, verbose_name='Title')
+    Completed = models.BooleanField(default=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.Title
