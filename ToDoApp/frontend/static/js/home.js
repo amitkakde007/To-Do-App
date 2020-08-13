a052768c9bc678ae8d49d3e8a84024e32daf3816
Amit Kakde <amitkakde911@outlook.com>
2020-08-13 12:49:05 +0530

    Create task functionality complete

--- ToDoApp/frontend/static/js/home.js
+++ ToDoApp/frontend/static/js/home.js
@@ -1,8 +1,27 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+
 function BuildList(){
     const wrapper =document.getElementById('list-wrapper')
-    let url='http://localhost:8000/api/tasklist/'
+    let fetchUrl='http://localhost:8000/api/tasklist/'
     
-    listData =fetch(url)
+    wrapper.innerHTML=''
+    listData =fetch(fetchUrl)
     .then((res)=>res.json())
     .then((data)=>{
         listData=data
@@ -24,5 +43,27 @@ function BuildList(){
         }
     })
 }
+    
+    form.addEventListener('submit',(e)=>{
+    let form = document.getElementById('form-wrapper')
+    let title = document.getElementById('title').value
+    
+    console.log(title)
+        e.preventDefault()
+        console.log('Form submitted')
+        postUrl='http://localhost:8000/api/taskcreate/'
+
+        fetch(postUrl,{
+            method:'POST',
+            headers:{
+                'Content-type':'application/json',
+                'X-CSRFToken':csrftoken,
+            },
+            body:JSON.stringify({'Title':title})
+        
+        }).then((res)=>{BuildList(),
+        document.getElementById('form').reset()})
+    })
+    
 
 BuildList()
